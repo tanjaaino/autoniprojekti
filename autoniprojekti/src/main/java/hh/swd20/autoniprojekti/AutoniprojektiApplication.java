d29cc99c9c17aef195bc09de822761de66c66483
Bergius Tanja <berta@haaga-helia.fi>
2019-09-01 19:14:00 +0300

    ekat muutokset

---
+++ autoniprojekti/src/main/java/hh/swd20/autoniprojekti/AutoniprojektiApplication.java
@@ -0,0 +1,13 @@
+package hh.swd20.autoniprojekti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoniprojektiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoniprojektiApplication.class, args);
+	}
+
+}
---
+++ autoniprojekti/src/test/java/hh/swd20/autoniprojekti/AutoniprojektiApplicationTests.java
@@ -0,0 +1,16 @@
+package hh.swd20.autoniprojekti;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AutoniprojektiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
